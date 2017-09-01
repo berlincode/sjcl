d42dcac7ee93d02fd1274588e07f013308608864
elastic <elastic.code@gmail.com>
2017-09-02 01:15:40 +0200

    increased version for pypi release

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ HISTORY = open('CHANGES.txt').read().replace('.. :changelog:', '')
 
 setup(
     name='sjcl',
-    version='0.1.6',
+    version='0.1.7',
     description="Decrypt and encrypt messages compatible to the 'Stanford Javascript Crypto Library (SJCL)' message format.",
     long_description=README + '\n\n' + HISTORY,
     author='Ulf Bartel',
